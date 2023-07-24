fe1ab123eaad24d84b5032ef02440fccab4fac93
am-i25 <201600062@student.polytechnic.bh>
2023-07-25 01:31:18 +0300

    finishing shit/ algo/fetch allo left

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './index.css';
 import Coo from './pet/pet.tsx';
 import Adoption from './Adoption/adopt.tsx';
 import Customize from './Customize/Customize.tsx'
 import Welcome from './welcome/Welcome.tsx';
-import { data } from './Adoption/adopt.tsx';
+// import { data } from './Adoption/adopt.tsx';
 
-const petData = [
-  {
-    id: "1",
-    name: "Buddy",
-    type: "Dog",
-    img: "dog",
-    description: "Friendly and playful lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    age: 3
-  },
-  {
-    id: "2",
-    name: "Whiskers",
-    type: "Cat",
-    img: "dog",
-    description: "Independent and curious lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    age: 2
-  },
-  {
-    id: "3",
-    name: "Max",
-    type: "Dog",
-    img: "dog",
-    description: "Loyal and protective lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    age: 4
-  },
-  {
-    id: "4",
-    name: "Simba",
-    type: "Cat",
-    img: "cat",
-    description: "Adventurous and energetic lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-    age: 1
-  },
-  {
-    id: "5",
-    name: "Bella",
-    type: "Dog",
-    img: "Cat",
-    description: "Affectionate and gentle lorem ipsum dolor sit amet consectetur adipisicing elit. Quisqua",
-    age: 5
-  },
-  {
-    id: "6",
-    name: "Luna",
-    
-    type: "Cat",
-    img: "Cat",
-    description: "Calm and graceful lorem ipsum dolor sit amet consectetur a",
-    age: 3
-  }
-];
+export interface data {
+  id: string; 
+  name: string;
+  type: string;
+  img: string;
+  description: string;
+  age: number;
+  points?:number;
+  size?: number;
+  activityLevel?: string;
+  goodWithKids?: boolean;
+  goodWithOtherPets?: boolean;
+}
 
-window.addEventListener('load', async () => {
-  const response = await fetch('/pets');
-  const pets = await response.json();
-  console.log(pets);
-});
-
-
+  
+type PetsResponse = data[];
 function App() {
   const [currentPage, setCurrentPage] = useState<string>('welcome');
   const [selectedPet, setSelectedPet] = useState<data | null>(null)
   const [selectedFilter, setSelectedFilter] = useState<string>('');
+  const [pets, setPets] = useState([]);
+
+
+    useEffect(() => {
+      let isMounted = true;
+      fetch('/pets')
+      .then((res) => res.json() as Promise<PetsResponse>)
+        .then((data) => {
+          if (isMounted) setPets(data);
+        });
+      return () => { isMounted = false };
+    }, []);
 
   const handleAdoptClick = () => {
     setCurrentPage('adoption');
@@ -105,7 +74,7 @@ const handleFilterChange = (choice: string) => {
         
       )}
       {currentPage === 'adoption' && (
-        <Adoption pets={petData} onBackClick={handleBackClick} onChooseClick={handlePetSelection} onFilterChange={handleFilterChange} selectedFilter={selectedFilter}/>
+        <Adoption pets={pets} onBackClick={handleBackClick} onChooseClick={handlePetSelection} onFilterChange={handleFilterChange} selectedFilter={selectedFilter}/>
 
       )} 
        {currentPage === 'customize' &&  (    
